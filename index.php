5792d092ddaff7060772310c3b0b4682df386beb
mr-lagb <115945829+mr-lagb@users.noreply.github.com>
2022-10-16 16:44:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// This program is free software; you can redistribute it and/or
+// This program is free software; you can redistribute it and/o
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
